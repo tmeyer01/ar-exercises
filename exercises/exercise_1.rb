64fd022b7e52c298a6b8a78b684357d7cc36e82f
thomas <tmeyerwebdev@gmail.com>
2021-11-09 23:34:02 +0000

    Add code to store class. Add Employee class

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,7 +6,11 @@ puts "----------"
 # Your code goes below here ...
 
 class Store < ActiveRecord::Base
+  has_many :employees
+end
 
+class Employee < ActiveRecord::Base
+  belongs_to :store
 end
 
 
