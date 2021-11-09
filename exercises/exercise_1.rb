a32b794026ea06bb5aad3ca0fe279d3f64bf5d28
thomas <tmeyerwebdev@gmail.com>
2021-11-09 22:13:18 +0000

    Add class method for stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,15 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+class Store < ActiveRecord::Base
+
+end
+
+
+burnaby = Store.create(name: "Burnanby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: true,  womens_apparel:true)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel:true)
+
+
+puts "Number of stores" + Store.count.to_s 
