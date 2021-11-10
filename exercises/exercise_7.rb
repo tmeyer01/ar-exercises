401e6a402a8875101c915a3e48f68909cdea3464
thomas <tmeyerwebdev@gmail.com>
2021-11-10 00:16:24 +0000

    add validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,25 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  #first name and last name presence 
+  validates :first_name, :last_name, presence: true 
+  # have a hourly_rate that is a number (integer) between 40 and 200
+  validates :hourly_rate, numericality: {only_integer: true, less_than_or_equal_to: 200, greater_than_or_equal_to: 40 }
+  #Employees must always have a store that they belong to 
+  belongs_to :store 
+end 
+
+
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+end
+
+puts "Enter name for a new store"
+user_answer = gets.chomp.to_s
+
+user_store = Store.create(name: user_answer)
+
+
